64004d7360d5fb181a979e49ef721bdcdace3d7d
Ralph <chaosskill@gmail.com>
2023-04-07 12:36:25 +0200

    mini change

---
+++ lab.sql
@@ -0,0 +1,24 @@
+-- Instructions
+-- Create a Python connection with SQL database and retrieve the results of the following queries as dataframes:
+-- How many distinct (different) actors' last names are there?
+
+
+
+-- Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
+
+
+-- Get all films with ARMAGEDDON in the title.
+
+
+-- Get 10 the longest films.
+
+
+-- How many films include Behind the Scenes content?
+
+
+-- Which kind of movies (rating) have a mean duration of more than two hours?
+
+
+-- Rank films by length (filter out the rows that have nulls or 0s in length column). 
+-- In your output, only select the columns title, length, and the rank.
+
